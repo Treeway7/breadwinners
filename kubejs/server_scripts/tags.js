bf1bd9d5c3cc24b8faf800e4d67e8a71021e7c6a
Treeway7 <56928485+Treeway7@users.noreply.github.com>
2023-08-23 17:52:25 -0500

    Convert line endings to LF

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -1,4 +1,4 @@
-ServerEvents.tags('item', event => {
+ServerEvents.tags('item', (event) => {
     // Seed Pouch
     event.add('quark:seed_pouch_holdable', 'farmersdelight:onion')
 
@@ -6,4 +6,4 @@ ServerEvents.tags('item', event => {
     event.add('forge:bread', ['autumnity:pumpkin_bread', 'neapolitan:banana_bread', 'reliquary:glowing_bread', 'some_assembly_required:burger_bun'])
 
     // Bread Slices
-})
+});
